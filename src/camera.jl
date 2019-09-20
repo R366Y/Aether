01d02c640014a74986155d289351cc47545d15c4
R366Y <nicomn@gmail.com>
2019-09-20 23:08:15 +0200

    Add CameraModule
    Add Camera struct

---
+++ src/camera.jl
@@ -0,0 +1,54 @@
+module CameraModule
+
+export Camera, ray_for_pixel
+
+using Aether.HomogeneousCoordinates
+using Aether.MatrixTransformations
+using Aether.Rays
+
+using LinearAlgebra
+using StaticArrays
+
+mutable struct Camera
+    hsize::Int16
+    vsize::Int16
+    field_of_view::Float64
+    transform::SMatrix{4,4,Float64}
+    half_width::Float64
+    half_height::Float64
+    pixel_size::Float64
+
+    function Camera(hsize::Int16, vsize::Int16, field_of_view::Float64)
+        half_view = tan(field_of_view / 2)
+        aspect = hsize / vsize
+        if aspect >= 1
+            half_width = half_view
+            half_height = half_view / aspect
+        else
+            half_width = half_view * aspect
+            half_height = half_view
+        end
+        pixel_size = (half_width * 2) / hsize
+        new(hsize, vsize, field_of_view, identity_matrix(),
+            half_width, half_height, pixel_size)
+    end
+end
+
+function ray_for_pixel(camera::Camera, px::Int16, py::Int16)
+    # the offset from the edge of the canvas to the pixel's center
+    xoffset = (px + 0.5) * camera.pixel_size
+    yoffset = (py + 0.5) * camera.pixel_size
+    # the untransformed coordinates of the pixel in world space.
+    # (remember that the camera looks toward -z, so +x is to the *left*.)
+    world_x = camera.half_width - xoffset
+    world_y = camera.half_height - yoffset
+    # using the camera matrix, transform the canvas point and the origin,
+    # and then compute the ray's direction vector.
+    # (remember that the canvas is at z=-1)
+    pixel = inverse(camera.transform) * point(world_x, world_y, -1)
+    origin = inverse(camera.transform) * point(0, 0, 0)
+    direction = normalize(pixel - origin)
+    return Ray(origin, direction)
+end
+
+end  # module
