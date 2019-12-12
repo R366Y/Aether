692b115c3801d569500da60b5f0d6b1affac7cda
R366y <nicomn@gmail.com>
2019-12-12 15:58:35 +0100

    WIP: Add yaml parser test
    Yaml importer to be implemented

---
+++ src/yaml_importer.jl
@@ -0,0 +1,29 @@
+module SceneImporters
+
+import YAML
+
+function import_yaml_scene_file(filename::String)
+
+end
+
+function parse_camera_data(yaml_data::Dict)
+
+end
+
+function parse_lights_data(yaml_data::Dict)
+
+end
+
+function parse_materials_data(yaml_data::Dict)
+
+end
+
+function parse_transforms_data(yaml_data::Dict)
+
+end
+
+function parse_objects_data(yaml_data::Dict)
+
+end
+
+end
