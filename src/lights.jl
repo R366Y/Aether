041d8eba7f1beed8911edd135c88ccdccecf6759
R366y <nicomn@gmail.com>
2020-01-07 15:51:40 +0100

    Add area lights implementation

--- src/lights.jl
+++ src/lights.jl
@@ -1,6 +1,6 @@
 module Lights
 
-export LightType, PointLight, default_point_light
+export LightType, AreaLight, PointLight, default_point_light, point_on_light
 
 import Base: ==
 using Aether.HomogeneousCoordinates
@@ -9,8 +9,29 @@ using Aether.ColorsModule
 abstract type LightType end
 
 struct PointLight <: LightType
-    position::Vec3D{Float64}
+    position::Vecf64
     intensity::ColorRGB
+end
+
+struct AreaLight <: LightType
+    position::Vecf64
+    intensity::ColorRGB
+    corner::Vecf64
+    uvec::Vecf64
+    usteps::Int
+    vvec::Vecf64
+    vsteps::Int
+    samples::Int
+
+    function AreaLight(corner::Vecf64, uvec::Vecf64, usteps::Int, 
+                       vvec::Vecf64, vsteps::Int, intensity::ColorRGB)
+        uvec = uvec / Float64(usteps)
+        vvec = vvec / Float64(vsteps)
+        samples = usteps * vsteps
+        position = corner + (uvec * (usteps * 0.5)) + (vvec * (vsteps * 0.5))
+        new(position, intensity, corner, uvec, usteps,
+            vvec, vsteps, samples) 
+    end
 end
 
 function default_point_light()
@@ -21,5 +42,11 @@ end
 
 @inline ==(p1::PointLight, p2::PointLight) =
     p1.position == p2.position && p1.intensity == p2.intensity
+
+function point_on_light(light::AreaLight, u::Int, v::Int)
+    return light.corner + 
+           light.uvec * (u + 0.5) +
+           light.vvec * (v + 0.5)
+end
 
 end
--- test/test_lights.jl
+++ test/test_lights.jl
@@ -11,4 +11,37 @@ using Test
         @test light.position == position
         @test light.intensity == intensity
     end
-end # begin
+
+    @testset "Creating an area light" begin
+        corner = point3D(0., 0., 0.)
+        v1 = vector3D(2., 0., 0.)
+        v2 = vector3D(0., 0., 1.)
+        light = AreaLight(corner, v1, 4, v2, 2, white)
+        @test light.corner == corner
+        @test light.uvec == vector3D(0.5, 0., 0.)
+        @test light.usteps == 4
+        @test light.vvec == vector3D(0., 0., 0.5)
+        @test light.vsteps == 2
+        @test light.samples == 8
+        @test light.position == point3D(1., 0., 0.5)
+    end
+
+    @testset "Finding a single point on an area light" begin
+        corner = point3D(0., 0., 0.)
+        v1 = vector3D(2., 0., 0.)
+        v2 = vector3D(0., 0., 1.)
+        light = AreaLight(corner, v1, 4, v2, 2, white)
+
+        input = NamedTuple[]
+        ks = (:u, :v, :result)
+        push!(input, NamedTuple{ks}((0, 0, point3D(0.25, 0., 0.25))))
+        push!(input, NamedTuple{ks}((1, 0, point3D(0.75, 0., 0.25))))
+        push!(input, NamedTuple{ks}((0, 1, point3D(0.25, 0., 0.75))))
+        push!(input, NamedTuple{ks}((2, 0, point3D(1.25, 0., 0.25))))
+        push!(input, NamedTuple{ks}((3, 1, point3D(1.75, 0., 0.75))))
+
+        for i in input
+            @test point_on_light(light, i.u, i.v) == i.result
+        end
+    end
+end 
