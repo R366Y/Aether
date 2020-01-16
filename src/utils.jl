2089858c2ad0cfd7cf40a10d759b4c6c5cd18177
R366y <nicomn@gmail.com>
2020-01-16 15:48:14 +0100

    Add lighting() function that samples area lights
    Add a new RandomGenerator

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,9 @@
 module Utils
 
 using Base.Iterators
+using Random
 
-export Generator, next!, push_tuple
+export Generator, RandomGenerator, next!, push_tuple
 
 mutable struct Generator 
     sequence
@@ -13,11 +14,27 @@ mutable struct Generator
     end 
 end
 
+mutable struct RandomGenerator
+    rng
+
+    function RandomGenerator()
+        new(MersenneTwister(1234))
+    end
+
+    function RandomGenerator(seed::Int)
+        new(MersenneTwister(seed))
+    end
+end
+
 function next!(g::Generator)
     n = iterate(g.sequence, g.next)
     element, state = n
     g.next = state
     return element
+end
+
+function next!(rg::RandomGenerator)
+    return rand(Float64, 1)[1]
 end
 
 @generated function push_tuple(x...)
